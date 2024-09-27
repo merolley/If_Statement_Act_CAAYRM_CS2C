e62431a42a838d714f89b409ddce2d0e67c72c3f
merolley <merolly.caay11@gmail.com>
2024-09-27 19:41:40 +0800

    initial commit

--- ageCheck.js
+++ ageCheck.js
@@ -4,13 +4,8 @@ let age = prompt("Enter your age:");
 // Checking the user's input using if-else statement
 if (age < 13) {
     console.log("You are a child");
-} else if (age >= 13) {
+} else if (age >= 13 && age <= 19) {
     console.log("You are a teenager");
-} else if (age <= 19) {
-    console.log("You are a teenager");
-} else {
+} else  {
     console.log("You are an adult");
 } 
-//else {
-  //  console.log("Please enter your age :>");
-//}
