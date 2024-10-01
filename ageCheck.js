715e47c77c20f88a6189ac81fc0105f0fd725920
merolley <merolly.caay11@gmail.com>
2024-10-01 21:48:33 +0800

    initial commit

--- ageCheck.js
+++ ageCheck.js
@@ -1,7 +1,18 @@
-//user's age
-let age = prompt("Enter your age:");
-
-// Checking the user's input using if-else statement
+/*references:
+https://www.enablegeek.com/tutorial/implementing-readline-for-user-input-in-node-js/
+https://www.shecodes.io/athena/36373-how-to-create-an-if-statement-in-javascript
+https://www.w3schools.com/JSREF/jsref_if.asp
+*/
+//creating age checker
+//implementing the readline for node js
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+//user's input for age
+readline.question("Enter your age: ", age => {
+    age = parseInt(age);
+//checking the input
 if (age < 13) {
     console.log("You are a child");
 } else if (age >= 13 && age <= 19) {
@@ -9,3 +20,6 @@ if (age < 13) {
 } else  {
     console.log("You are an adult");
 } 
+
+    readline.close();
+});
